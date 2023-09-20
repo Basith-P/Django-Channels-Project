698e6e484636d82b6e477ea23429bafc18a04c0c
Basith P <basithpofficial@gmail.com>
2023-09-20 22:58:41 +0530

    feat: add comments

--- stats/consumers.py
+++ stats/consumers.py
@@ -4,28 +4,28 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class DashboardConsumer(AsyncWebsocketConsumer):
+    # This method is called when a WebSocket connection is established.
     async def connect(self):
+        # Get the 'dashboard_slug' from the URL route's keyword arguments.
         dashboard_slug = self.scope["url_route"]["kwargs"]["dashboard_slug"]
         self.dashboard_slug = dashboard_slug
         await self.accept()
 
+    # This method is called when a WebSocket connection is closed.
     async def disconnect(self, close_code):
-        # await self.channel_layer.group_discard(
-        #     "dashboard",
-        #     self.channel_name
-        # )
         print(f"disconnected with code: {close_code}")
 
-    async def stats_update(self, event):
-        await self.send_json(event)
-
+    # This method is called when the server receives a message from a client.
     async def receive(self, text_data):
+        # Parse the received JSON message into Python objects.
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         sender = text_data_json["sender"]
 
+        # Print the received message and sender for debugging purposes.
         print(f"received message: {message} from {sender}")
 
+        # Send a response message back to the client.
         await self.send(text_data=json.dumps({
             "message": message,
             "sender": sender
--- stats/static/stats/dashboard.js
+++ stats/static/stats/dashboard.js
@@ -1,25 +1,35 @@
+// Get the 'dashboardSlug' from the HTML element with the ID 'dashboard-slug'.
 const dashboardSlug = document
   .getElementById("dashboard-slug")
   .textContent.trim();
-console.log("dashboardSlug: ", dashboardSlug);
 
+// Create a WebSocket connection to the server using the obtained 'dashboardSlug'.
 const socket = new WebSocket(
   `ws://${window.location.host}/ws/${dashboardSlug}/`
 );
+
 console.log(socket);
 
+// This function is called when the WebSocket connection is successfully opened.
 socket.onopen = function (e) {
   console.log("Chat socket opened");
+
+  // Send a JSON message to the server once the connection is open.
   socket.send(
     JSON.stringify({ message: "Hello from the client!", sender: "test-client" })
   );
 };
 
+// This function is called when a message is received from the server.
 socket.onmessage = function (e) {
+  // Parse the received JSON data into a JavaScript object.
   const data = JSON.parse(e.data);
+
+  // Log the received data from the server for debugging purposes.
   console.log("Server: ", data);
 };
 
+// This function is called when the WebSocket connection is closed unexpectedly.
 socket.onclose = function (e) {
   console.error("Chat socket closed unexpectedly");
 };
